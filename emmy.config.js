fd3c1e49c5fd76e27d930b5b2118a9fe4c642a79
Emmanuel Norambuena <eanorambuena@uc.cl>
2026-04-14 18:38:10 -0400

    build: update application artifacts and components

--- emmy.config.js
+++ emmy.config.js
@@ -1,16 +1,13 @@
-// emmy.config.ts
-
-import { createConfig, javascript, Emmy } from 'emmy-dom/server'
+import { createConfig, Emmy } from 'emmy-dom/server'
 
 // Importa tus utilidades necesarias
 import { saveMarkdown } from './emmydocs'
 import * as markdown from './app/Markdown'
 import { app } from './app/App'
 
-// 1. Primero inyectas el Markdown en el objeto Emmy
 await saveMarkdown(Emmy, Object.keys(markdown))
 
-// 2. Y luego corres la configuración, la cual detectará y armará los componentes automáticamente
+// Configuración, la cual detectará y armará los componentes automáticamente
 await createConfig({
   // source: Carpeta donde viven todos tus web components y hooks a exportar
   source: './app',
